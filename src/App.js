3455df88ed455185c9a8487a7d94e456eea10f7a
Peng You <ypenggg11@gmail.com>
2023-02-28 21:11:28 +0000

    GET request with fetch and async

--- src/App.js
+++ src/App.js
@@ -5,25 +5,29 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  /* 10.- GET request with fetch API */
-  const fetchMoviesHandler = () => {
-    fetch("https://swapi.py4e.com/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((object) => {
-        const recoveredMovies = object.results.map((movie) => {
-          return {
-            id: movie.episode_id,
-            title: movie.title,
-            releaseDate: movie.release_date,
-            openingText: movie.opening_crawl,
-          };
-        });
+  /* 10.- GET request with fetch API && await & async */
+  const fetchMoviesHandler = async () => {
+    setIsLoading(true);
 
-        setMovies(recoveredMovies);
+    /* Way 2: await & async */
+    const response = await fetch("https://swapi.py4e.com/api/films/");
+
+    /* Way 1: Promises */
+    response.json().then((object) => {
+      const recoveredMovies = object.results.map((movie) => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          releaseDate: movie.release_date,
+          openingText: movie.opening_crawl,
+        };
       });
+
+      setMovies(recoveredMovies);
+      setIsLoading(false);
+    });
   };
 
   return (
@@ -32,7 +36,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>No movies found...</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
